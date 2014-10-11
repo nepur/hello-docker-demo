febed4bf0941fc73bcbfd31c940e64f3afc1ddb2
gte959f <rupenp@yahoo.com>
2014-10-11 18:26:10 -0400

    Mods for latest presentation

--- node2/src/index.js
+++ node2/src/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Hello Docker Node2');
+    res.send('Hello Docker Node2 - Change it back');
 });
 
 app.listen(8080);
